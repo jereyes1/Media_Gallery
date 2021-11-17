dd5e8a65b424e82c4077799f78d3fc60af04db32
jereyes1 <70930622+jereyes1@users.noreply.github.com>
2021-11-16 21:37:51 -0500

    Finished Media Gallary
    Button in cards are fixed by putting it into a div class. Background color and hover effects were added. New images for the media cards as well.

--- js/page.js
+++ js/page.js
@@ -36,16 +36,16 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
-// Get the button that opens the modal
+
 var btn = document.querySelectorAll("button.modal-button");
 
-// All page modals
+
 var modals = document.querySelectorAll('.modal');
 
-// Get the <span> element that closes the modal
+
 var spans = document.getElementsByClassName("close");
 
-// When the user clicks the button, open the modal
+
 for (var i = 0; i < btn.length; i++) {
  btn[i].onclick = function(e) {
     e.preventDefault();
@@ -54,7 +54,7 @@ for (var i = 0; i < btn.length; i++) {
  }
 }
 
-// When the user clicks on <span> (x), close the modal
+
 for (var i = 0; i < spans.length; i++) {
  spans[i].onclick = function() {
     for (var index in modals) {
@@ -63,7 +63,7 @@ for (var i = 0; i < spans.length; i++) {
  }
 }
 
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
     if (event.target.classList.contains('modal')) {
      for (var index in modals) {
